0f206f73e2ea8a0f0f9777671791782b859296ee
zero2smart <fivewisdomzero@gmail.com>
2018-11-23 13:30:53 -0500

    add javascript for pricing

--- src/Objs/Homepage/HomepageComponent.js
+++ src/Objs/Homepage/HomepageComponent.js
@@ -113,7 +113,7 @@ Scrivito.provideComponent("Homepage", ({ page }) => {
                 <div className="d-flex flex-column flex-md-row align-items-center" id="navbar">
                     <img className="my-0 mr-md-auto" src={require("../../assets/logo.png")}/>
                     <nav className="my-2 my-md-0 mr-md-3 navbar-font">
-                        <a className="p-4 text-dark log-in" href="#pricing">Your Pricing</a>
+                        <a className="p-4 text-dark log-in" id="your-pricing" href="#pricing">Your Pricing</a>
                         <a className="mt-4 text-dark pricing" href="https://talent-ninja.com/home/auth/login">Log In</a>
                     </nav>
                     <a id="btn-1254" href="https://talent-ninja.com/home/auth/signup" target="_self"><span>Create An Account</span></a>
